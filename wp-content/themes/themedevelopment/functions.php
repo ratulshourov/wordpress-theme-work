127a64b647325cb1a5dc252245ae1df9fa3ed301
Md.Ratul Uddin Ashraf <ratul@jrfintel.com>
2023-03-01 16:30:28 +0600

    main css add in functions.php

--- wp-content/themes/themedevelopment/functions.php
+++ wp-content/themes/themedevelopment/functions.php
@@ -1,4 +1,12 @@
 <?php 
 
 add_theme_support('title-tag');
+
+// custom css and js
+
+
+function add_css_js_calling() {
+wp_enqueue_style('main-style',get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts','add_css_js_calling');
 ?>
--- wp-content/themes/themedevelopment/index.php
+++ wp-content/themes/themedevelopment/index.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head();?>
 </head>
-<body>
+<body class="<?php body_class();?>">
+    
+
+    
 <?php wp_footer();?>
 </body>
 </html>
