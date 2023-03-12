abb83c51aaa216cf004c144e4d1b21d8673f450b
Md.Ratul Uddin Ashraf <ratul@jrfintel.com>
2023-03-12 16:14:40 +0600

    stylesheet add

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -1,6 +1,8 @@
 <?php
 function add_theme_scripts() {
-	wp_enqueue_style('style', get_stylesheet_uri() );
+   
+
+	wp_enqueue_style('style', get_stylesheet_uri() ,[],filemtime(get_template_directory()));
 
 	//wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all' );
 
