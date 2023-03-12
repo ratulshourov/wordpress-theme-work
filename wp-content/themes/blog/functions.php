2a6547e2c4552901adb38ae10925f00edb23f72a
Md.Ratul Uddin Ashraf <ratul@jrfintel.com>
2023-03-12 16:40:39 +0600

    custom css and js file enqueue and register

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -6,9 +6,15 @@ function add_theme_scripts() {
 
 	//wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all' );
 
-	//wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), 1.1, true );
+	wp_enqueue_script( 'asset-script', get_template_directory_uri() . '/assets/js/main.js', array(),filemtime(get_template_directory()) );
 
-	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+
+    wp_register_style('custom-css',get_template_directory().'/css/custom.css',[],filemtime(get_template_directory()));
+    wp_enqueue_style('custom-css');
+
+    wp_register_script('custom-js',get_template_directory().'/js/main.js',[],filemtime(get_template_directory()));
+	wp_enqueue_script('custom-js');
+    // if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 	// 	wp_enqueue_script( 'comment-reply' );
 	// }
 }
